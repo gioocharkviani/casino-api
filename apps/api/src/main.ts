64911f8f4e7800b584280df3259f15dc5092e778
gioocharkviani <gioocharkviani@gmail.com>
2026-06-11 01:17:35 +0400

    last chance

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: 'http://localhost:3000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Cookie', 'Authorization'],
