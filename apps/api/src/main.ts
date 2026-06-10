3d396175b5240293f02701745fcbc8c432694d7d
gioocharkviani <gioocharkviani@gmail.com>
2026-06-11 01:12:23 +0400

    change corse

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,12 +8,12 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // app.enableCors({
-  //   origin: t,
-  //   credentials: true,
-  //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  //   allowedHeaders: ['Content-Type', 'Cookie', 'Authorization'],
-  // });
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Cookie', 'Authorization'],
+  });
   app.use(cookieParser());
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
