913d2655776e4a7d0f794d3e76472ee0a614ca30
gioocharkviani <gioocharkviani@gmail.com>
2026-06-11 12:14:01 +0400

    fea

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,13 +8,13 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: true,
-    credentials: false,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Accept', 'Authorization', 'Cookie'],
-    exposedHeaders: ['Set-Cookie'],
-  });
+  // app.enableCors({
+  //   origin: true,
+  //   credentials: false,
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  //   allowedHeaders: ['Content-Type', 'Accept', 'Authorization', 'Cookie'],
+  //   exposedHeaders: ['Set-Cookie'],
+  // });
   app.use(cookieParser());
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
