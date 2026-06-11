1fde9b17a06d4ed85662f307362e96b44a1d67fa
gioocharkviani <gioocharkviani@gmail.com>
2026-06-11 09:24:25 +0400

    gr

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -12,14 +12,9 @@ async function bootstrap() {
     origin: 'http://localhost:3000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: [
-      'Content-Type',
-      'Accept',
-      'session_token',
-      'Authorization',
-    ],
-    exposedHeaders: ['Set-Cookie'],
+    allowedHeaders: true,
   });
+
   app.use(cookieParser());
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
