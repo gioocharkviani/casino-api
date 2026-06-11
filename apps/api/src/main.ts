9e2935d746c0d764f53ddac501bdef21019232ad
gioocharkviani <gioocharkviani@gmail.com>
2026-06-11 12:15:29 +0400

    trying to enagle all corse

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,13 +8,12 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // app.enableCors({
-  //   origin: true,
-  //   credentials: false,
-  //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  //   allowedHeaders: ['Content-Type', 'Accept', 'Authorization', 'Cookie'],
-  //   exposedHeaders: ['Set-Cookie'],
-  // });
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization', 'Cookie'],
+    exposedHeaders: ['Set-Cookie'],
+  });
   app.use(cookieParser());
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
