054131f9202044cb64390214b53f55e31926e664
gioocharkviani <gioocharkviani@gmail.com>
2026-06-23 11:57:35 +0400

    Greg

--- apps/wallet/src/main.ts
+++ apps/wallet/src/main.ts
@@ -3,33 +3,17 @@ import { WalletModule } from './wallet.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(WalletModule);
-
-  app.enableCors({
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: [
-      'Content-Type',
-      'Accept',
-      'Authorization',
-      'Cookie',
-      'X-API-Key',
-      'X-Merchant-Id',
-      'X-Idempotency-Key',
-      'User-Agent',
-    ],
-    exposedHeaders: ['Set-Cookie'],
-  });
-
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.TCP,
-    options: {
-      host: 'localhost',
-      port: 3031,
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    WalletModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: 'localhost',
+        port: 3031,
+      },
     },
-  });
-
-  await app.startAllMicroservices();
+  );
   console.log('WALLET MODULE START SUCCESSFULLY');
-  await app.listen(3000);
+  await app.listen();
 }
 bootstrap();
